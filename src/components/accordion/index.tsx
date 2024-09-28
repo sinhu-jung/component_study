a6f94f0ff17ce822d79b1a963639520e778e50ab
sinhu-jung <tlsgn26@naver.com>
2024-09-28 17:31:35 +0900

    css 로 아코디언 처리

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,13 +1,15 @@
 import Accordion1 from "./1_r";
+import Accordion2 from "./2_r";
 import cx from "./cx";
 
 const Accordions = () => {
-    return (
-        <div className={cx('Accordions')}>
-            <h2>아코디언</h2>
-            <Accordion1 />
-        </div>
-    )
-}
+  return (
+    <div className={cx("Accordions")}>
+      <h2>아코디언</h2>
+      <Accordion1 />
+      <Accordion2 />
+    </div>
+  );
+};
 
 export default Accordions;
