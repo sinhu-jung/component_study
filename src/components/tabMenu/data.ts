4509fb0e10ee019f2beb4c6a71f63a862b2674cf
sinhu-jung <tlsgn26@naver.com>
2024-09-29 10:47:08 +0900

    tab menu react로 만들어보기

---
+++ src/components/tabMenu/data.ts
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: "66f2918d51020402b219ea92",
+    title:
+      "In incididunt labore ea ut non. Anim nulla incididunt proident id ut.",
+    description:
+      "In officia adipisicing labore incididunt dolor nostrud consectetur labore. Voluptate pariatur proident exercitation cupidatat incididunt labore commodo eiusmod ex dolor. Incididunt et deserunt nisi exercitation reprehenderit. Deserunt nulla amet consectetur velit sunt proident laboris. Elit anim laboris ut ex deserunt minim non laboris voluptate.\r\nDo tempor sunt commodo sit labore consectetur ea ea reprehenderit occaecat elit culpa dolore ad. Eiusmod aute non in excepteur minim. Aliqua mollit proident est exercitation consequat voluptate qui cillum. Nisi sit consectetur aute aliquip id velit exercitation aliqua velit minim ea ullamco incididunt. Et aliquip ullamco aliquip culpa sit duis occaecat eiusmod cupidatat non ut nulla anim fugiat. Magna duis enim commodo laboris ullamco ut duis in.\r\nAmet consectetur sit irure proident dolore. Dolor consequat qui minim do cupidatat sunt fugiat do anim et. Consectetur pariatur ad ipsum pariatur est duis cillum minim et occaecat mollit.\r\nDolor incididunt eiusmod qui cupidatat consequat cupidatat fugiat nostrud labore. Sint mollit magna sit velit Lorem aliqua voluptate. Duis minim ullamco fugiat irure. Lorem voluptate ex proident nulla anim minim duis culpa consequat fugiat ex dolore. In irure sunt laboris laborum irure Lorem laboris fugiat cillum.\r\n",
+  },
+  {
+    id: "66f2918df8ea75b020fe5e01",
+    title: "Non eiusmod ut exercitation nulla consequat in eiusmod.",
+    description:
+      "Deserunt exercitation labore cupidatat sint consectetur elit do ex aliqua proident ea voluptate elit aute. Anim nisi proident occaecat laborum ut ad. Anim proident Lorem ullamco eiusmod in cillum sint consequat consequat. Eiusmod mollit ad dolor sunt amet ad consectetur incididunt eiusmod laboris aliqua nostrud pariatur aute. Esse veniam do esse duis eu ex ut excepteur veniam officia. Qui culpa amet consequat eu velit veniam duis laborum.\r\nEsse occaecat cupidatat fugiat proident irure. Anim cillum fugiat quis in aliquip qui in cupidatat incididunt non commodo dolore. Ipsum enim minim ipsum aliquip ex nulla. Excepteur amet ut elit fugiat laboris adipisicing aute est Lorem aute cupidatat. Ullamco labore amet ut in laborum reprehenderit. Reprehenderit fugiat anim sint aute minim Lorem culpa sit. Laborum anim tempor voluptate pariatur laborum.\r\nNulla deserunt aliquip dolore enim dolor laborum sit consequat cillum esse ipsum dolor magna aliquip. Cupidatat ullamco exercitation proident labore ut pariatur exercitation duis est nostrud amet. Cillum laborum deserunt commodo tempor laboris. Id aliquip laboris deserunt quis laborum occaecat ea et consectetur. Voluptate adipisicing proident dolore culpa. Ipsum id eu nulla elit consequat ea.\r\n",
+  },
+  {
+    id: "66f2918d85eec51f4b76b7e9",
+    title:
+      "Est incididunt ad commodo non dolor labore magna fugiat nisi id ipsum.",
+    description:
+      "Fugiat exercitation ad reprehenderit eiusmod elit laborum mollit. Amet mollit amet labore aliquip reprehenderit deserunt ullamco est minim sunt cupidatat. Magna quis duis laboris ullamco. Minim labore laborum voluptate deserunt dolore quis sunt occaecat. Elit laborum veniam fugiat Lorem nulla irure occaecat mollit id excepteur ad cupidatat. Nostrud et anim officia et irure aliquip cillum elit eiusmod fugiat qui eu ex aute.\r\nCillum officia non nisi elit nostrud voluptate et sit deserunt velit. Aliquip nulla quis proident non amet excepteur occaecat exercitation officia pariatur tempor et. Incididunt laboris veniam magna tempor cillum proident ut eu nostrud anim duis ex consequat. Irure nostrud amet proident aliqua aliquip eu cupidatat minim labore. Anim quis do Lorem aliquip ex id. Velit qui eu duis fugiat mollit ex est et est commodo. Enim enim aute reprehenderit exercitation do sunt duis nulla fugiat eiusmod tempor cupidatat.\r\nNulla commodo veniam est do sunt. Mollit et velit occaecat aliquip ullamco aliqua. Eu amet nostrud velit qui amet culpa eu. Voluptate officia fugiat anim esse consequat amet amet id officia dolor. Mollit reprehenderit occaecat minim et ullamco Lorem exercitation et culpa consectetur officia consequat nulla. Reprehenderit consequat do ipsum qui magna exercitation dolor ex officia officia veniam reprehenderit consectetur. Cillum do excepteur culpa cupidatat.\r\nAute enim sunt ea aliqua sunt commodo esse pariatur nulla cupidatat consequat sint. Reprehenderit non ex amet cupidatat veniam anim ea magna fugiat ullamco cupidatat veniam velit ex. Quis eu ipsum veniam aute deserunt Lorem nostrud pariatur non laborum sit consequat. Consectetur deserunt cupidatat nostrud eiusmod cupidatat excepteur mollit sunt laborum ex velit ullamco fugiat.\r\n",
+  },
+  {
+    id: "66f2918d85aabbba2c28d7ec",
+    title:
+      "Esse cillum labore amet tempor ad laborum. Quis ea excepteur anim tempor eiusmod incididunt duis ipsum mollit elit.",
+    description:
+      "Do non consectetur mollit non enim nulla occaecat ex esse sit et quis. Qui duis laboris mollit excepteur officia sint. Eiusmod nisi aliqua pariatur est qui eu.\r\nId consequat consequat culpa veniam. Sunt amet elit proident occaecat proident quis eiusmod fugiat quis sit labore culpa. Velit consequat ad do aliquip ex. Excepteur qui cillum elit reprehenderit occaecat non aliqua ut id ex nostrud ex.\r\nExercitation anim anim minim cillum fugiat voluptate consectetur esse nostrud nisi sunt. Tempor ut fugiat duis in ipsum aute enim incididunt. Duis dolore fugiat eiusmod ipsum non cillum dolore culpa qui irure anim. Eiusmod cupidatat minim magna eiusmod et eiusmod reprehenderit consequat enim exercitation sint. Velit nostrud nisi reprehenderit quis excepteur.\r\nDuis sint deserunt dolor ut consequat minim nostrud veniam ut quis sunt irure culpa do. Officia commodo excepteur excepteur commodo nulla aliquip excepteur magna occaecat excepteur sint aliquip aliqua consequat. Ex ullamco irure ad labore velit id voluptate. Ut ut reprehenderit tempor eiusmod amet aliquip id enim consequat eiusmod excepteur veniam. Pariatur aliqua reprehenderit veniam esse anim minim Lorem dolore eu dolor in ad.\r\n",
+  },
+];
+
+export default data;
